307f0b1532a0392757ed1686bff76b50c52227ed
citizen63000 <citizen.lantien@gmail.com>
2023-01-06 19:30:23 +0100

    empty php files

--- www/index.php
+++ www/index.php
@@ -1,4 +1,11 @@
 <?php
 
-	echo 'youpi!';
+require_once __DIR__.'/vendor/autoload.php';
+
+$username = 'zz3f3';
+$password = 'easyma';
+$server =
+$client = new MongoDB\Client('mongodb+srv://$username:$password@'.$_ENV['ATLAS_CLUSTER_SRV'].'/test');
+
+
 
