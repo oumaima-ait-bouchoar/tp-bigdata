71db977f5750fa4dd4032988db493cec6e839bf7
citizen63000 <citizen.lantien@gmail.com>
2023-01-16 20:55:19 +0100

    help for create

--- www/public/create.php
+++ www/public/create.php
@@ -1,2 +1,25 @@
 <?php
-echo 'insérez le contenu du formulaire (variable $_POST) dans la base et retournez sur la liste';
+
+include_once '../init.php';
+
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+$twig = getTwig();
+$manager = getMongoDbManager();
+
+// @todo implementez la récupération des données d'une entité et la passer au template
+// petite aide : https://github.com/VSG24/mongodb-php-examples
+
+if (!empty($_POST)) {
+    // @todo coder l'enregistrement d'un nouveau livre en lisant le contenu de $_POST
+} else {
+// render template
+    try {
+        echo $twig->render('create.html.twig');
+    } catch (LoaderError|RuntimeError|SyntaxError $e) {
+        echo $e->getMessage();
+    }
+}
+
